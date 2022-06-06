ec23ffca6f6d7f705291620c2c4ba07a67764648
Ram Panpalia <85726711+RamPanpalia@users.noreply.github.com>
2022-06-06 10:23:21 +0530

    Google button fixed

--- src/Login.js
+++ src/Login.js
@@ -1,115 +1,129 @@
-import React,{useState,useEffect} from 'react';
-// import {GoogleLogin,GoogleLogout} from 'react-google-login'
+import React, { useState, useEffect } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 // import GoogleButton from 'react-google-button'
-import './Login.css'
-import {db} from './fire'
-import {collection,getDocs} from "firebase/firestore";
+import "./Login.css";
+import { db } from "./fire";
+import { collection, getDocs } from "firebase/firestore";
 
-export default function login(props){
-
-    const translatelogin=()=>{
-        var a=document.querySelector('.Login').style
-        if(a.transform==="translateY(-2000px)"){
-            a.transform="translateY(0px)";
-        }
-        else{
-            a.transform="translateY(-2000px)";
-        }
+export default function login(props) {
+  const translatelogin = () => {
+    var a = document.querySelector(".Login").style;
+    if (a.transform === "translateY(-2000px)") {
+      a.transform = "translateY(0px)";
+    } else {
+      a.transform = "translateY(-2000px)";
     }
+  };
 
-    const [users,setUsers]=useState([])
-    const usersCollectionref=collection(db,"users");
-    useEffect(()=>{
+  const [users, setUsers] = useState([]);
+  const usersCollectionref = collection(db, "users");
+  useEffect(() => {
+    const getUser = async () => {
+      const data = await getDocs(usersCollectionref);
+      console.log(data);
+    };
+    getUser();
+  }, []);
 
-        const getUser=async()=>{
-            const data=await getDocs(usersCollectionref)
-            console.log(data)
-        }
-        getUser()
-
-    },[])
-
-    const {
-        email,
-        setEmail,
-        password,
-        setPassword,
-        handleLogin,
-        handleSignUp,
-        hasAccount,
-        setHasAccount,
-        emailError,
-        passwordError,
-    }=props;
-    return (
-        <>
-        <div className='Login'>
-            <div className='part1 part'>
-                <div className='login'>Login</div>
-                <div className='whylogin'>Get access to your Orders, Wishlist and Recommendations</div>
-                <div className='Login-img'>
-                    <img src='./sutt.png' alt=''/>
-                    SuKart
-                </div>
-            </div>
-            <div className='part2 part'>
-                <div className='closeLogin' onClick={translatelogin}>
-                    <div className='close-login-bar close-login-bar1'></div>
-                    <div className='close-login-bar close-login-bar2'></div>
-                </div>
-                <div className='email'>
-                    <input
-                    placeholder='Name'
-                    type='text'
-                    autoFocus
-                    required
-                    />
-                    <input 
-                     placeholder='Email Address'
-                     type='text'
-                     autoFocus
-                     required
-                     value={email}
-                     onChange={(e)=>setEmail(e.target.value)}
-                    />
-                    <p className='errMsg'>{emailError}</p>
-                </div>
-                <div className='password'>
-                    <input 
-                     placeholder='Password' 
-                     type="password"
-                     required
-                     value={password}
-                     onChange={(e)=>setPassword(e.target.value)}
-                    />
-                    <p className='errMsg'>{passwordError}</p>
-                </div>
-                <div className='btn-container'>
-                    {hasAccount?(
-                    <>
-                    <div className='Login-btn' onClick={handleLogin}>Sign in</div>
-                    <p className='hasAccQuery'>Don't have an Account ?
-                        <span onClick={()=>setHasAccount(!hasAccount)}> Sign up</span>
-                    </p>
-                    </>
-                    ):(
-                    <>
-                    <div className='Login-btn' onClick={handleSignUp}>Sign up</div>
-                    <p className='hasAccQuery'>Already have an Account ?
-                        <span onClick={()=>setHasAccount(!hasAccount)}> Sign in</span>
-                    </p>
-                    </>
-                    )}
-                    <p><u>or</u></p>
-                    {/* <GoogleButton
-                    onClick={() => { console.log('Google button clicked') }}
-                    /> */}
-                    {/* <GoogleLogin></GoogleLogin> */}
-                    <div className='Login-btn google-login'>Login with Google</div>
-                </div>
-                <div className='onlyForBITSian'>Only for BITSians, <br/>Login with BITS ID only</div>
-            </div>
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+    handleSignUp,
+    hasAccount,
+    setHasAccount,
+    emailError,
+    passwordError,
+  } = props;
+  return (
+    <>
+      <div className="Login">
+        <div className="part1 part">
+          <div className="login">Login</div>
+          <div className="whylogin">
+            Get access to your Orders, Wishlist and Recommendations
+          </div>
+          <div className="Login-img">
+            <img src="./sutt.png" alt="" />
+            SuKart
+          </div>
         </div>
-        </>
-    )
+        <div className="part2 part">
+          <div className="closeLogin" onClick={translatelogin}>
+            <div className="close-login-bar close-login-bar1"></div>
+            <div className="close-login-bar close-login-bar2"></div>
+          </div>
+          <div className="email">
+            <input placeholder="Name" type="text" autoFocus required />
+            <input
+              placeholder="Email Address"
+              type="text"
+              autoFocus
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p className="errMsg">{emailError}</p>
+          </div>
+          <div className="password">
+            <input
+              placeholder="Password"
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <p className="errMsg">{passwordError}</p>
+          </div>
+          <div className="btn-container">
+            {hasAccount ? (
+              <>
+                <div className="Login-btn" onClick={handleLogin}>
+                  Sign in
+                </div>
+                <p className="hasAccQuery">
+                  Don't have an Account ?
+                  <span onClick={() => setHasAccount(!hasAccount)}>
+                    {" "}
+                    Sign up
+                  </span>
+                </p>
+              </>
+            ) : (
+              <>
+                <div className="Login-btn" onClick={handleSignUp}>
+                  Sign up
+                </div>
+                <p className="hasAccQuery">
+                  Already have an Account ?
+                  <span onClick={() => setHasAccount(!hasAccount)}>
+                    {" "}
+                    Sign in
+                  </span>
+                </p>
+              </>
+            )}
+            <p>
+              <u>or</u>,
+            </p>
+            <div className="google-login">
+                <GoogleLogin
+                  clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                  buttonText="Login"
+                  //   onSuccess={responseGoogle}
+                  //   onFailure={responseGoogle}
+                  cookiePolicy={"single_host_origin"}
+                />
+            </div>
+          </div>
+          <div className="onlyForBITSian">
+            Only for BITSians, <br />
+            Login with BITS ID only
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
