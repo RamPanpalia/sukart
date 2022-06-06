eb3339e59fe2610624a6d3e232a87f1f24c4e47d
Ram Panpalia <85726711+RamPanpalia@users.noreply.github.com>
2022-06-06 10:28:02 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -111,7 +111,7 @@ export default function login(props) {
             <div className="google-login">
                 <GoogleLogin
                   clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                  buttonText="Login"
+                  buttonText="Login with Google"
                   //   onSuccess={responseGoogle}
                   //   onFailure={responseGoogle}
                   cookiePolicy={"single_host_origin"}
