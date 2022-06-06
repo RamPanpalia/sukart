dd3a8e493756f613daf2470c2df34d44269387a0
panpalia_ram <panpaliaram@gmail.com>
2022-06-06 16:29:05 +0530

    .

--- src/Carousal.js
+++ src/Carousal.js
@@ -13,14 +13,14 @@ const Carousal = () => {
     <>
     <div className='Navigator'>
         <div className='LeftArrow'>
-        <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-            <path d="M20 44 0 24 20 4 22.8 6.85 5.65 24 22.8 41.15Z"/>
-        </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+              <path d="M20 44 0 24 20 4 22.8 6.85 5.65 24 22.8 41.15Z"/>
+          </svg>
         </div>
         <div className='RightArrow'>
-        <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-            <path d="M15.2 43.9 12.4 41.05 29.55 23.9 12.4 6.75 15.2 3.9 35.2 23.9Z"/>
-        </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+              <path d="M15.2 43.9 12.4 41.05 29.55 23.9 12.4 6.75 15.2 3.9 35.2 23.9Z"/>
+          </svg>
         </div>
     </div>
     <div className='Carousal'>
