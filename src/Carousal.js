747b0838c2a97538da5bce6462cdf3ccd26263e4
panpalia_ram <panpaliaram@gmail.com>
2022-06-06 18:03:46 +0530

    .

--- src/Carousal.js
+++ src/Carousal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import './Carousal.css'
 import img1 from './assets/Carousal/Cimg1.webp'
 import img2 from './assets/Carousal/Cimg2.jpg'
@@ -9,21 +9,38 @@ import img6 from './assets/Carousal/Cimg6.jpg'
 import img7 from './assets/Carousal/Cimg7.jpg'
 
 const Carousal = () => {
+
+    const ref=useRef(null);
+    const scrollIt=(x,y,ele)=>{
+        // ref.current.scrollLeft+=x;
+        // document.querySelector(ele).scrollRight+=y;
+        // document.querySelector(ele).scrollLeft+=x;
+        // document.querySelector(ele).scrollRight+=y;
+    }
+
+    function scroll_It(x,y,ele){
+        // document.querySelector(ele).scrollLeft+=x;
+        // document.querySelector(ele).scrollRight+=y;
+    }
+
   return (
-    <>
+    <div className='carousal-main'>
+
+    <div className='Carousal'>
+
     <div className='Navigator'>
-        <div className='LeftArrow'>
-          <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-              <path d="M20 44 0 24 20 4 22.8 6.85 5.65 24 22.8 41.15Z"/>
-          </svg>
+        <div className='LeftArrow' onClick={scroll_It(1300,0,".Carousal")} >
+         <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+             <path d="M20 44 0 24 20 4 22.8 6.85 5.65 24 22.8 41.15Z"/>
+         </svg>
         </div>
-        <div className='RightArrow'>
-          <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-              <path d="M15.2 43.9 12.4 41.05 29.55 23.9 12.4 6.75 15.2 3.9 35.2 23.9Z"/>
-          </svg>
+        <div className='RightArrow' onClick={scrollIt(0,1300,'.Carousal')}>
+         <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+             <path d="M15.2 43.9 12.4 41.05 29.55 23.9 12.4 6.75 15.2 3.9 35.2 23.9Z"/>
+         </svg>
         </div>
     </div>
-    <div className='Carousal'>
+
         <img src={img1} alt=''/>
         <img src={img2} alt=''/>
         <img src={img3} alt=''/>
@@ -32,7 +49,7 @@ const Carousal = () => {
         <img src={img6} alt=''/>
         <img src={img7} alt=''/>
     </div>
-    </>
+    </div>
   )
 }
 
