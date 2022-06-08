591cd2857b80b1c15103817034a12ce0dd6aa45f
panpalia_ram <panpaliaram@gmail.com>
2022-06-09 01:56:04 +0530

    Login, Logout and Signup Done

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,6 +7,16 @@ import { auth, db } from "./FirebaseConfigs/firebaseConfig";
 import { collection, getDocs, query, where } from "firebase/firestore";
 
 export default function Navbar() {
+    
+  const translatelogin = () => {
+    const a = document.querySelector(".Login").style;
+    if (a.transform === "translateY(-2000px)") {
+      a.transform = "translateY(0px)";
+    } else {
+      a.transform = "translateY(-2000px)";
+    }
+  };
+
   function GetCurrentUser() {
     const [user, setUser] = useState("");
     const usersCollectionRef = collection(db, "users");
@@ -31,36 +41,18 @@ export default function Navbar() {
     return user
   }
 
-  const [userName,setUserName]=useState("user");
   const loggeduser = GetCurrentUser();
-  var abc="";
   if(loggeduser){
       console.log(loggeduser[0].username);
-    //   abc=loggeduser[0].username;
   }
-//   setUserName(abc);
+
   const [profileimg, setProfileImg] = useState(null);
 
-  const translatelogin = () => {
-    const a = document.querySelector(".Login").style;
-    if (a.transform === "translateY(-2000px)") {
-      a.transform = "translateY(0px)";
-    } else {
-      a.transform = "translateY(-2000px)";
-    }
-  };
-  // const loggedOutState=()=>{
-  //     document.querySelector('.Cart').style.display="none";
-  //     document.querySelector('.Favourites').style.display="none";
-  //     document.querySelector('.Log-out').style.display="none";
-  //     document.querySelector('.Log-in').style.display="block";
-  // }
-  // const loggedInState=()=>{
-  //     document.querySelector('.Cart').style.display="block";
-  //     document.querySelector('.Favourites').style.display="block";
-  //     document.querySelector('.Log-out').style.display="block";
-  //     document.querySelector('.Log-in').style.display="none";
-  // }
+  const handleLogout=()=>{
+      auth.signOut().then(()=>{
+          translatelogin();
+      })
+  }
 
   return (
     <>
@@ -80,6 +72,8 @@ export default function Navbar() {
           <img src={profileimg ? profileimg : profileLogo} alt="" />
           <span>Hi {loggeduser?loggeduser[0].username:"user"},</span>
         </div>
+        {loggeduser?
+        <>
         <div className="Cart nav-ele">
           <svg
             id="Layer_1"
@@ -91,14 +85,21 @@ export default function Navbar() {
             <title>cart</title>
             <path d="M4.06,8.22A4.15,4.15,0,0,1,0,4.06,4.13,4.13,0,0,1,4.06,0h6A19.12,19.12,0,0,1,20,2.6c5.44,3.45,6.41,8.38,7.8,13.94h91a4.07,4.07,0,0,1,4.06,4.06,5,5,0,0,1-.21,1.25L112.06,64.61a4,4,0,0,1-4,3.13H41.51c1.46,5.41,2.92,8.32,4.89,9.67C48.8,79,53,79.08,59.93,79h47.13a4.06,4.06,0,0,1,0,8.12H60c-8.63.1-13.94-.11-18.2-2.91s-6.66-7.91-8.95-17h0L18.94,14.46c0-.1,0-.1-.11-.21a7.26,7.26,0,0,0-3.12-4.68A10.65,10.65,0,0,0,10,8.22H4.06Zm80.32,25a2.89,2.89,0,0,1,5.66,0V48.93a2.89,2.89,0,0,1-5.66,0V33.24Zm-16.95,0a2.89,2.89,0,0,1,5.67,0V48.93a2.89,2.89,0,0,1-5.67,0V33.24Zm-16.94,0a2.89,2.89,0,0,1,5.66,0V48.93a2.89,2.89,0,0,1-5.66,0V33.24Zm41.72-8.58H30.07l9.26,34.86H105l8.64-34.86Zm2.68,67.21a10,10,0,1,1-10,10,10,10,0,0,1,10-10Zm-43.8,0a10,10,0,1,1-10,10,10,10,0,0,1,10-10Z" />
           </svg>
+          {loggeduser[0].cart}
         </div>
-        <div className="Favourites nav-ele">Favourites</div>
+        </>:<></>
+        }
+        {loggeduser?
+        <div className="Favourites nav-ele">Favourites</div>:<></>}
+        {loggeduser?
+        <div className="Log-out nav-ele" onClick={handleLogout}>
+          Log-out
+        </div>
+        :
         <div className="Log-in nav-ele" onClick={translatelogin}>
           Log-in
         </div>
-        <div className="Log-out nav-ele" onClick="">
-          Log-out
-        </div>
+        }
         <div className="closeNavbar-for-small-devices">
           <span className="material-symbols-outlined">expand_more</span>
         </div>
