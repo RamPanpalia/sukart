4848d79ddbd6171effa88df3ddfa6e0761cb52ed
panpalia_ram <panpaliaram@gmail.com>
2022-06-17 22:42:44 +0530

    Section0.js updated

--- src/Section0.js
+++ src/Section0.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { 
     collection, 
     query, 
     onSnapshot, 
-    getDocs 
+    getDocs, 
+    QuerySnapshot
 } from 'firebase/firestore'
 import { db } from './FirebaseConfigs/firebaseConfig'
 import './Section0.css'
 
-const Section0 = (props) => {
-   alert(props.producttype)
+const Section0 = () => {
+    const [products,setProducts]=useState([]);
+    useEffect(()=>{
+        const getProducts=()=>{
+            const productsArray=[];
+            const path=`products-${("BITS MERCHANDISE")}`;
+            // alert(path);
+            getDocs(collection(db,path)).then((querySnapshot)=>{
+                querySnapshot.forEach((doc)=>{
+                    productsArray.push({...doc.data(),id:doc.id})
+                })
+            });
+        }
+        getProducts();
+    },[])
   return (
-    <div>Section0</div>
+    <>
+        <div>Section0</div>
+
+    </>
   )
 }
 
